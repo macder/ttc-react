b19f8e51f6ad73a943546170b1d94c481063d3c4
Maciej Derulski <macder7@gmail.com>
2017-07-13 22:17:34 -0400

    webpack - use constant for paths

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,10 @@
 const path = require('path');
 
-// ui build root
-const buildPath = path.resolve(process.cwd(), 'build');
-
 module.exports = {
   entry: './src/main.jsx',
   output: {
     filename: 'bundle.js',
-    path: buildPath
+    path: __dirname + '/build'
   },
   module: {
     rules: [
