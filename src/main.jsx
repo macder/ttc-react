beb669d6c48cf9dc0d9cbcf30dc1cdffd17229d2
Maciej Derulski <macder7@gmail.com>
2017-07-20 20:46:07 -0400

    entry point - simplification

--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,10 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import ScheduleSelect from './containers/ScheduleSelect.jsx';
 
-class Main extends React.Component {
 
-  render() {
-    return (
-      <div>
-        <h1>TTC NextBus</h1>
-        <ScheduleSelect />
-      </div>
-    );
-  }
-}
-
-ReactDOM.render(<Main />, document.getElementById('root'));
+ReactDOM.render(
+  <div>
+    <h1>TTC NextBus</h1>
+    <ScheduleSelect />
+  </div>, document.getElementById('root')
+);
