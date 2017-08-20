cbbfb5fbdeec74ce965d4b797df19a2954c1f369
Maciej Derulski <macder7@gmail.com>
2017-08-20 02:00:32 -0400

    search/saga - fetches http and parses routeList

--- src/modules/search/sagas.js
+++ src/modules/search/sagas.js
@@ -1,26 +1,45 @@
 import "regenerator-runtime/runtime";
+import axios from 'axios';
+
 import { delay } from 'redux-saga'
 import { call, put, takeEvery, takeLatest } from 'redux-saga/effects'
 
 import * as t from './actionTypes';
 import * as actions from './actions';
+import xml2js from 'xml2js';
 
-function* fetch () {
-  yield delay(1000);
-  return [
-    {text: '5-Avenue Road', value: '5'},
-    {text: '6-Bay', value: '6'},
-    {text: '7-Bathurst', value: '7'},
-    {text: '8-Broadview', value: '8'},
-  ];
+function* fetch (url, callback) {
+  let data = {};
+
+  yield axios({
+    method: 'get',
+    url: url,
+    responseType: 'text'
+  })
+    .then(function(response) {
+      data = response.data;
+  });
+  return data;
 }
 
 // worker Saga: will be fired on LOAD_ROUTES_REQUEST actions
 function* fetchRoutes(action) {
   try {
-    // const list = yield call(Api.fetchRoutes, 'arg1', 'arg2');
-    const list = yield call(fetch);
+    const url = 'http://webservices.nextbus.com/service/publicXMLFeed?command=routeList&a=ttc';
+    const options = {
+      trim: true,
+      mergeAttrs: true,
+      explicitArray: false,
+    };
 
+    const data = parseXML(yield call(fetch, url), options).body.route;
+
+    const list = data.map(function(obj) {
+      return {
+        text: obj.title,
+        value: obj.tag
+      }
+    });
     yield put(actions.loadRoutesSuccess(list));
 
   } catch (e) {
@@ -33,7 +52,16 @@ function* fetchRoutes(action) {
   Allows concurrent fetches.
 */
 function* loadRouteList() {
-  yield takeEvery(t.LOAD_ROUTES_REQUEST, fetchRoutes);
+  yield takeLatest(t.LOAD_ROUTES_REQUEST, fetchRoutes);
+}
+
+function parseXML(data, options) {
+  let parsed = {};
+
+  xml2js.parseString(data, options, (err, result) => {
+    parsed = result;
+  });
+  return parsed;
 }
 
 export default loadRouteList;
