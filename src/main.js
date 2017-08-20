8e4b4dbf80d75c4e1c73025cd95d960d5d9b4b7c
Maciej Derulski <macder7@gmail.com>
2017-08-19 20:06:44 -0400

    attach saga middleware to redux store

--- src/main.js
+++ src/main.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
+import createSagaMiddleware from 'redux-saga'
+
 import reducer from './rootReducer'
 import search from './modules/search';
 
-import {connect} from 'react-redux';
+const sagaMiddleware = createSagaMiddleware();
 
-let store = createStore(reducer);
+let store = createStore(
+  reducer,
+  applyMiddleware(sagaMiddleware)
+);
+
+sagaMiddleware.run(search.sagas.default);
 
 const SearchForm = search.containers.default.SearchFormContainer;
 
