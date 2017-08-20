987da1f4c4c53bcc739f523126d64da68546edb2
Maciej Derulski <macder7@gmail.com>
2017-08-19 23:04:14 -0400

    action for when getting routeList fails

--- src/modules/search/actions.js
+++ src/modules/search/actions.js
@@ -1,5 +1,13 @@
 import * as t from './actionTypes';
 
+export const loadRoutesFailure = () => {
+  return {
+    type: t.LOAD_ROUTES_FAILURE,
+    fetching: false,
+    populated: false,
+  }
+};
+
 export const loadRoutesRequest = () => {
   return {
     type: t.LOAD_ROUTES_REQUEST,
