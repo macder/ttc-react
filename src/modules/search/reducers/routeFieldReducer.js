c60b84400c08a46dce4763b94ff6069fad953c43
Maciej Derulski <macder7@gmail.com>
2017-08-23 02:09:19 -0400

    reducer for selected route

--- src/modules/search/reducers/routeFieldReducer.js
+++ src/modules/search/reducers/routeFieldReducer.js
@@ -6,6 +6,11 @@ const initialState = {
 
 const routeFieldReducer = (state = initialState, action = {}) => {
   switch (action.type) {
+    case t.SELECTED_ROUTE:
+      return Object.assign({}, state, {
+        selected: action.selected
+      });
+
     default:
       return state
   }
