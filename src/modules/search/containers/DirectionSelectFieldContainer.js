16167f16d0fc042c1e37880be94d93f398475429
Maciej Derulski <macder7@gmail.com>
2017-08-26 02:49:51 -0400

    WIP direction field props from state and some cleanup

--- src/modules/search/containers/DirectionSelectFieldContainer.js
+++ src/modules/search/containers/DirectionSelectFieldContainer.js
@@ -4,38 +4,36 @@ import {connect} from 'react-redux';
 
 import DirectionSelectField from '../components/DirectionSelectField';
 
-import * as action from '../actions.js'
+import { getDirectionList } from '../selectors'
+// import * as action from '../actions.js'
 
 class DirectionSelectFieldContainer extends React.Component {
   constructor(props) {
     super(props);
-    //this.handleDirectionSelect = this.handleDirectionSelect.bind(this);
+    this.handleDirectionSelect = this.handleDirectionSelect.bind(this);
   }
 
   componentDidMount() {
-
+    //console.dir(this.props);
   }
 
   componentWillReceiveProps(nextProps) {
-    // console.log('DirectionSelectFieldContainer new props');
+    //console.dir(nextProps);
+  }
+
+  componentWillUpdate(nextProps, nextState) {
+    console.log('DirectionSelectFieldContainer will update');
   }
 
   handleDirectionSelect(value) {
     console.log(value);
-    // this.props.dispatch(action.selectedDirection(value));
   }
 
   render() {
-    const directionList = [
-      /* {text: title, value: tag} */
-      {text: 'North - 5 Avenue Rd towards Eglinton Station', value: '5_1_5B'},
-      {text: 'South - 5b Avenue Rd towards Gerrard', value: '5_0_5B'},
-    ];
-
     return (
       <div>
         <DirectionSelectField
-          list = {directionList}
+          list = {this.props.list}
           onSelected = {this.handleDirectionSelect}
         />
       </div>
@@ -49,11 +47,17 @@ DirectionSelectFieldContainer.propTypes = {
 }
 
 const mapStateToProps = (state) => {
-  console.log(state);
   return {
-    // list: state.searchState.data.routeList.payload,
-    // state: state.searchState.routeField
-  }
+    list: getDirectionList(state),
+  };
 }
 
-export default connect(mapStateToProps)(DirectionSelectFieldContainer);
+const mapDispatchToProps = (dispatch) => {
+  return {
+    action: {
+      // loadRouteConfig: bindActionCreators(loadRouteConfigRequest, dispatch)
+    }
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(DirectionSelectFieldContainer);
