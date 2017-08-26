87f6187eeaed4c7a423ec4bff17dd7d7fab5a66b
Maciej Derulski <macder7@gmail.com>
2017-08-26 02:46:45 -0400

    search - selector to get list of directions from store

--- src/modules/search/selectors.js
+++ src/modules/search/selectors.js
@@ -3,12 +3,30 @@ import { createSelector } from 'reselect'
 const routeList = state => state.searchState.data.routeList.payload
 const routeConfig = state => state.searchState.data.routeConfig.payload
 
+// Get route list array for 'Route' autocomplete field
 export const getRouteList = createSelector(
   [routeList],
   (list) => list
 );
 
+// Get complete route config object
 export const getRouteConfig = createSelector(
   [routeConfig],
   (config) => config
+);
+
+// Get direction list array for 'Direction' autocomplete field
+export const getDirectionList = createSelector(
+  [routeConfig],
+  (config) => {
+    if(config.direction) {
+      return config.direction.map(function(obj) {
+        return {
+          id: obj.tag,
+          title: obj.title,
+        }
+      });
+    }
+    return []
+  }
 );
