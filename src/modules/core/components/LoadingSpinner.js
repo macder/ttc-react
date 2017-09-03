0e75832720e3c27c3b7da346dfa9ff36a8785f4e
Maciej Derulski <macder7@gmail.com>
2017-09-03 17:21:53 -0400

    position loading spinner

--- src/modules/core/components/LoadingSpinner.js
+++ src/modules/core/components/LoadingSpinner.js
@@ -2,10 +2,14 @@ import React from 'react';
 import CircularProgress from 'material-ui/CircularProgress';
 
 export default function LoadingSpinner(props) {
+  const style = {
+    marginTop: 24
+  }
   return (
     <CircularProgress
       size={24}
       thickness={3}
+      style={style}
     />
   );
 }
