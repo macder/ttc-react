65a09e2f9332d7eb952a778274d9b594a63710f9
Maciej Derulski <macder7@gmail.com>
2017-09-04 19:14:59 -0400

    Predictions - saga to http fetch prediction data

---
+++ src/modules/predictions/sagas.js
@@ -0,0 +1,52 @@
+import "regenerator-runtime/runtime";
+
+import { all, call, put, takeEvery } from 'redux-saga/effects'
+
+import * as t from './actionTypes';
+import * as actions from './actions';
+import {httpGet} from '../../services/httpRequest';
+import {parseXML} from '../../services/parsers';
+
+/**
+ * Worker Saga: will be fired on
+ *  LOAD_PREDICTIONS_REQUEST actions
+ * @param {object} args
+ * @param {object} action - redux action
+ */
+function* fetch(args, action){
+  try {
+    const options = {
+      trim: true,
+      mergeAttrs: true,
+      explicitArray: false,
+    };
+
+    const data = parseXML(yield call(httpGet, action.url), options).body.predictions.direction.prediction;
+
+    // console.dir(data);
+
+    yield put(actions[args.success](data));
+
+  } catch (e) {
+    yield put(actions[args.fail](e));
+  }
+}
+
+/**
+ * Starts fetch on each dispatched `LOAD_PREDICTIONS_REQUEST` action.
+ * Allows concurrent fetches.
+ *
+ */
+function* loadPredictions() {
+  const args = {
+    success: 'loadPredictionsSuccess',
+    fail: 'loadPredictionsFailure',
+  };
+  yield takeEvery(t.LOAD_PREDICTIONS_REQUEST, fetch, args);
+}
+
+export default function* rootSaga() {
+  yield all ([
+    loadPredictions(),
+  ])
+}
