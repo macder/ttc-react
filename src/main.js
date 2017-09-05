37f5677134a2c1ffec23be00e559aac54e8a9420
Maciej Derulski <macder7@gmail.com>
2017-09-04 20:44:42 -0400

    main - temp comment out immutablejs - not in use yet

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Immutable from 'immutable';
+// import Immutable from 'immutable';
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
 import createSagaMiddleware from 'redux-saga'
