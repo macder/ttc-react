8a37a305bc176becbd1201ace0e136a586f127e1
Maciej Derulski <macder7@gmail.com>
2017-09-04 20:06:25 -0400

    Predictions - selector to get route and stop id

--- src/modules/predictions/selectors.js
+++ src/modules/predictions/selectors.js
@@ -1,9 +1,24 @@
 import { createSelector } from 'reselect'
 
+const searchState = state => state.searchState
 const predictionList = state => state.predictionState.payload
 
 // Get prediction payload
 export const getPredictions = createSelector(
   [predictionList],
   (list) => list
+);
+
+// Get selected route and stop ID's, or return null
+export const getRouteStopId = createSelector(
+  [searchState],
+  (search) => {
+    if(search.routeField.selected && search.stopField.selected){
+      return {
+        routeId: search.routeField.selected,
+        stopId: search.stopField.selected
+      };
+    }
+    return null;
+  }
 );
