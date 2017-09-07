5f208fff8a2b2fdfcdd2b1e63dabfc04bf4c16db
Maciej Derulski <macder7@gmail.com>
2017-09-07 00:56:35 -0400

    Start of reducer refactor to reduce/eliminate code duplication

--- src/modules/predictions/reducer.js
+++ src/modules/predictions/reducer.js
@@ -6,36 +6,57 @@ const initialState = {
   payload: null,
 };
 
+const updateObject = (oldObject, newValues) => {
+  // Encapsulate the idea of passing a new object as the first parameter
+  // to Object.assign to ensure we correctly copy data instead of mutating
+  return Object.assign({}, oldObject, newValues);
+}
+
+const loadPredictionsRequest = (state, action) => {
+  return updateObject(state, {
+    fetching: action.fetching,
+    visible: action.visible,
+  });
+}
+
+const loadPredictionsSuccess = (state, action) => {
+  return updateObject(state, {
+    fetching: false,
+    payload: action.payload,
+    visible: action.visible,
+  });
+}
+
+const loadPredictionsFailure = (state, action) => {
+  return updateObject(state, {
+    fetching: false,
+    payload: action.payload,
+    visible: action.visible,
+    error: action.error
+  });
+}
+
+const clearPredictions = (state, action) => {
+  return updateObject(state, {
+    fetching: false,
+    payload: action.payload,
+    visible: action.visible,
+  });
+}
+
 const predictionsReducer = (state = initialState, action = {}) => {
   switch (action.type) {
-
     case t.LOAD_PREDICTIONS_REQUEST:
-      return Object.assign({}, state, {
-        fetching: action.fetching,
-        visible: action.visible,
-      });
+      return loadPredictionsRequest(state, action);
 
     case t.LOAD_PREDICTIONS_SUCCESS:
-      return Object.assign({}, state, {
-        fetching: false,
-        payload: action.payload,
-        visible: action.visible,
-      });
+      return loadPredictionsSuccess(state, action);
 
     case t.LOAD_PREDICTIONS_FAILURE:
-      return Object.assign({}, state, {
-        fetching: false,
-        payload: action.payload,
-        visible: action.visible,
-        error: action.error
-      });
+      return loadPredictionsFailure(state, action);
 
     case t.CLEAR_PREDICTIONS:
-      return Object.assign({}, state, {
-        fetching: false,
-        payload: action.payload,
-        visible: action.visible,
-      });
+      return clearPredictions(state, action);
 
     default:
       return state
