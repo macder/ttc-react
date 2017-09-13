974e773c0a14fe981bcbfe9d6e85ccbc299cead0
Maciej Derulski <macder7@gmail.com>
2017-09-13 01:46:49 -0400

    file clean up

--- src/modules/search/reducers/directionFieldReducer.js
+++ src/modules/search/reducers/directionFieldReducer.js
@@ -7,42 +7,10 @@ const initialState = Immutable.fromJS({
   // input: null,
 });
 
-const updateObject = (oldObject, newValues) =>
-  // Encapsulate the idea of passing a new object as the first parameter
-  // to Object.assign to ensure we correctly copy data instead of mutating
-  Object.assign({}, oldObject, newValues);
-
-
-const selectedDirection = (state, action) => updateObject(state, {
-  selected: action.selected,
-});
-
-const selectedRoute = state => updateObject(state, {
-  selected: null,
-  visible: true,
-  input: null,
-});
-
-const clearRoute = (state, action) => updateObject(state, {
-  selected: action.selected,
-  input: action.input,
-  visible: false,
-});
-
-const clearDirection = (state, action) => updateObject(state, {
-  selected: action.selected,
-  input: action.input,
-});
-
-const inputDirection = (state, action) => updateObject(state, {
-  input: action.input,
-});
-
 const directionFieldReducer = (state = initialState, action = {}) => {
   switch (action.type) {
 
     case t.CLEAR_ROUTE:
-      // console.log('clear route in direction reducer');
       return state
         .set('visible', false)
         .set('selected', null);
@@ -50,22 +18,14 @@ const directionFieldReducer = (state = initialState, action = {}) => {
     case t.SELECTED_ROUTE:
       return state
         .set('visible', true);
-      // return selectedRoute(state);
 
     case t.SELECTED_DIRECTION:
-      // console.log('SELECTED_DIRECTION');
       return state
         .set('selected', action.selected);
-      // return selectedDirection(state, action);
-
-    /*case t.SELECTED_ROUTE:
-      return selectedRoute(state);
 
     case t.CLEAR_DIRECTION:
-      return clearDirection(state, action);
-
-    case t.INPUT_DIRECTION:
-      return inputDirection(state, action);*/
+      return state
+        .set('selected', action.selected);
 
     default:
       return state;
