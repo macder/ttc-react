d81b495d93a42a5eb7906d37d90cdeea08fd82ec
Maciej Derulski <macder7@gmail.com>
2017-09-22 02:03:24 -0400

    WIP - data formatting for predictions payload from API

--- src/modules/predictions/sagas.js
+++ src/modules/predictions/sagas.js
@@ -1,5 +1,6 @@
 import 'regenerator-runtime/runtime';
 
+import Immutable from 'immutable';
 import { all, call, put, takeEvery } from 'redux-saga/effects';
 
 import * as t from './actionTypes';
@@ -21,11 +22,81 @@ function* fetch(args, action) {
       explicitArray: false,
     };
 
-    const data = parseXML(yield call(httpGet, action.url), options).body.predictions;
-    yield put(actions[args.success](data));
+    // const data = parseXML(yield call(httpGet, action.url), options).body.predictions;
+    const data = yield call(httpGet, action.url);
+
+    // console.dir(data);
+
+    const test = yield call(format, data.predictions);
+
+    console.dir(test);
+
+    // yield put(actions[args.success](data));
   } catch (e) {
     yield put(actions[args.fail](e));
   }
+}
+
+function format(payload) {
+  // console.dir(payload);
+
+  const PredictionRecord = new Immutable.Record({
+    affectedByLayover: 'false',
+    block: '',
+    branch: '',
+    dirTag: '',
+    epochTime: '',
+    isDeparture: '',
+    minutes: '',
+    seconds: '',
+    tripTag: '',
+    vehicle: '',
+  });
+
+  if (payload.direction) {
+    // console.log('has predictions');
+    //console.dir(payload.direction);
+    // const test = Immutable.fromJS(payload.direction);
+    // console.dir(test);
+
+    if (Array.isArray(payload.direction)) {
+      //console.log('is array');
+      //console.dir(payload.direction);
+
+      const data = Immutable.fromJS(payload.direction);
+
+      // console.dir(data);
+
+      return new Immutable.List(data.map(item => {
+        //console.dir(item);
+        let prediction;
+
+        if(Immutable.Map.isMap(item.get('prediction'))) {
+          console.log('is map');
+          // const prediction = new Immutable.OrderedSet(item.get('prediction')).map(PredictionRecord);
+          // const prediction = item.get('prediction').map(PredictionRecord);
+          prediction = new PredictionRecord(item.get('prediction'));
+          // console.dir(prediction);
+        } else {
+          prediction = new Immutable.OrderedSet(item.get('prediction')).map(PredictionRecord);
+        }
+
+        // console.dir(prediction);
+        // console.dir(item.get('prediction'));
+
+        return new Immutable.Map({
+          title: item.get('title'),
+          // prediction: new Immutable.OrderedSet(item.get('prediction')).map(PredictionRecord),
+          prediction: prediction,
+        })
+      }));
+      // console.dir(lorem);
+
+    }
+
+  }
+
+  return {hello: 'world'}
 }
 
 /**
