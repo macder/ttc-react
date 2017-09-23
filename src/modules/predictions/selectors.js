0da79c5b2dd6e6e945c2e9ac34910bc164803925
Maciej Derulski <macder7@gmail.com>
2017-09-23 02:18:48 -0400

    Prediction selectors refactor to immutablejs

--- src/modules/predictions/selectors.js
+++ src/modules/predictions/selectors.js
@@ -1,91 +1,34 @@
 import { createSelector } from 'reselect';
 
-const searchState = state => state.searchState;
-const payload = state => state.predictionState.payload;
-const visible = state => state.predictionState.visible;
+const searchState = state => state.get('searchState');
+const predictionState = state => state.get('predictionState');
 
 // Get prediction payload
-const predictions = createSelector(
-  [payload],
-  (data) => {
-    console.log('*------createSelector--------');
-    console.dir(data);
-    console.log('------createSelector------*');
-    if (data && Object.prototype.hasOwnProperty.call(data, 'direction')) {
-      // multi direction predictions - eg 12a and 12b
-      if (Array.isArray(data.direction)) {
-        return data.direction.map((value) => {
-          const result = (Array.isArray(value.prediction))
-            ? value.prediction
-            : [value.prediction];
-          return {
-            prediction: result,
-            title: value.title,
-          };
-        });
-      }
-
-      // stop has single set of predictions for route
-      if (Array.isArray(data.direction.prediction)) {
-        return [data.direction];
-      }
-
-      // only 1 prediction - last vehicle?
-      return [
-        {
-          title: data.direction.title,
-          prediction: [
-            data.direction.prediction,
-          ],
-        },
-      ];
-      // return [];
-    }
-    else if (data && Object.prototype.hasOwnProperty.call(data, 'dirTitleBecauseNoPredictions')) {
-      return [];
-    }
-    return null;
-  },
+export const getPrediction = createSelector(
+  [predictionState],
+  (prediction) => prediction.get('payload')
 );
 
-export const getPredictions = createSelector(
-  [predictions],
-  data => data,
-);
-
-export const getPredictionsInMinutes = createSelector(
-  [predictions],
-  data => (
-    data ? data.map(value => value.prediction.map(row => row.minutes)) : null
-  ),
-);
-
-// Check if the route stop has predictions
-export const hasPredictions = createSelector(
-  [payload],
-  data => (
-    data
-      ? !(Object.prototype.hasOwnProperty.call(data, 'dirTitleBecauseNoPredictions'))
-      : false
-  ),
-);
-
-// Get selected route and stop ID's, or return null
-export const getRouteStopId = createSelector(
+// Get selected route
+export const getRoute = createSelector(
   [searchState],
-  (search) => {
-    if (search.routeField.selected && search.stopField.selected) {
-      return {
-        routeId: search.routeField.selected,
-        stopId: search.stopField.selected,
-      };
-    }
-    return null;
-  },
+  (search) => search.getIn(['routeField', 'selected'])
 );
 
-// Check if visible
+// Get selected stop
+export const getStop = createSelector(
+  [searchState],
+  (search) => search.getIn(['stopField', 'selected'])
+);
+
+// Is prediction data fetching from remote api? bool
+export const isFetching = createSelector(
+  [predictionState],
+  (prediction) => prediction.get('fetching')
+);
+
+// Is component visible? bool
 export const isVisible = createSelector(
-  [visible],
-  result => result,
+  [predictionState],
+  (prediction) => prediction.get('visible')
 );
