69d690c8b481b4b968d064849a31cbcaa6acd312
Maciej Derulski <macder7@gmail.com>
2017-09-25 20:51:41 -0400

    PredictionsContainer - new base boilerplate

--- src/modules/predictions/containers/PredictionsContainer.js
+++ src/modules/predictions/containers/PredictionsContainer.js
@@ -2,26 +2,20 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import {List, ListItem} from 'material-ui/List';
-import Subheader from 'material-ui/Subheader';
-// import Divider from 'material-ui/Divider';
 import Predictions from '../components/Predictions2';
 // import { getPrediction, getRoute, getStop, isFetching, isVisible } from '../selectors';
 import * as selector from '../selectors';
 import { loadPredictionsRequest, clearPredictions } from '../actions';
 
 
-function mapStateToProps(state) {
-  return {
-    // toDoApp: state.toDoApp // gives our component access to state through props.toDoApp
-  }
-}
 
-function mapDispatchToProps(dispatch) {
-  return {
+const mapStateToProps = state => ({
+  test: 'hello'
+});
 
-  }; // here we're mapping actions to props
-}
+const mapDispatchToProps = dispatch => ({
+
+});
 
 export default connect(
   mapStateToProps,
