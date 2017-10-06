79d7413dc1c46847529449f3139f11abfbb0cfe9
Maciej Derulski <macder7@gmail.com>
2017-10-06 07:34:34 -0400

    Predictions - request data dispatch action working!!!

--- src/modules/predictions/containers/PredictionsContainer.js
+++ src/modules/predictions/containers/PredictionsContainer.js
@@ -1,25 +1,50 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-import Predictions from '../components/Predictions2';
-// import { getPrediction, getRoute, getStop, isFetching, isVisible } from '../selectors';
-import * as selector from '../selectors';
-import { loadPredictionsRequest, clearPredictions } from '../actions';
+import { compose, lifecycle } from 'recompose';
+// import Predictions from '../components/Predictions2';
+import LoadingSpinner from '../../core/components/LoadingSpinner';
+import { getPrediction, getRoute, getStop, isFetching, isVisible } from '../selectors';
+import { clearPredictions, loadPredictionsRequest } from '../actions';
 
+const shouldFetchData = props =>
+  (props.route && props.stop && !props.payload && !props.fetching) ? true : false
 
-
-const mapStateToProps = state => ({
-  test: 'hello'
+const withPredictionData = lifecycle({
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.requestData) {
+      nextProps.requestData();
+    }
+  }
 });
 
-const mapDispatchToProps = dispatch => ({
+const enhance = compose(
+  connect(
+    (state) => ({
+      payload: getPrediction(state),
+      route: getRoute(state),
+      stop: getStop(state),
+      fetching: isFetching(state),
+    }),
+    (dispatch) => ({
+      requestFetch: (route, stop) => {dispatch(loadPredictionsRequest(route, stop))}
+    }),
+    (stateProps, dispatchProps, ownProps) => ({
+      requestData: (shouldFetchData(stateProps))
+        ? () => dispatchProps.requestFetch(stateProps.route, stateProps.stop)
+        : null,
+      payload: stateProps.payload,
+      fetching: stateProps.fetching
+    })
+  ),
+  withPredictionData,
+);
 
-});
+const Predictions = props => (
+  <div className="c-predictions">
+    <p>predictcions test</p>
+  </div>
+);
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(Predictions);
+export default enhance(Predictions);
 
-// PredictionsContainer
