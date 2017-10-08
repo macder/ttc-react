8ab901e95f0afcd4e3c026b8788dcc45c18a02ae
Maciej Derulski <macder7@gmail.com>
2017-10-08 06:06:53 -0400

    remove unnecessary return

--- src/modules/predictions/components/PredictionsEmpty.js
+++ src/modules/predictions/components/PredictionsEmpty.js
@@ -2,13 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Immutable from 'immutable';
 
-const PredictionsEmpty = props => {
-  return (
-    <div className="c-predictions">
-      <p>No predictions available</p>
-    </div>
-  );
-}
+// TEMP
+const PredictionsEmpty = props => (
+  <div className="c-predictions">
+    <p>No predictions available</p>
+  </div>
+);
+
 
 PredictionsEmpty.propTypes = {
 };
