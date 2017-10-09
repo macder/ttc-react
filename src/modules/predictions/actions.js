f3a186a384202a2674d507c91bed464ff2eecb7c
Maciej Derulski <macder7@gmail.com>
2017-10-09 04:00:08 -0400

    formatting fix

--- src/modules/predictions/actions.js
+++ src/modules/predictions/actions.js
@@ -5,15 +5,14 @@ import * as t from './actionTypes';
  * @return {object}
  */
 export const loadPredictionsRequest = (routeId, stopId) => ({
-    type: t.LOAD_PREDICTIONS_REQUEST,
-    fetching: true,
-    url: `http://webservices.nextbus.com/service/publicJSONFeed?command=predictions&a=ttc&r=${routeId}&s=${stopId}`,
-    // url: '/data/prediction_2dirs_multi.json',
-    // url: '/data/prediction_1dir_single.json',
-    // url: '/data/prediction_2dirs_1multi_1single.json',
-    // url: '/data/prediction_1dir_multi.json',
-    // url: '/data/prediction_nonr.json',
-
+  type: t.LOAD_PREDICTIONS_REQUEST,
+  fetching: true,
+  url: `http://webservices.nextbus.com/service/publicJSONFeed?command=predictions&a=ttc&r=${routeId}&s=${stopId}`,
+  // url: '/data/prediction_2dirs_multi.json',
+  // url: '/data/prediction_1dir_single.json',
+  // url: '/data/prediction_2dirs_1multi_1single.json',
+  // url: '/data/prediction_1dir_multi.json',
+  // url: '/data/prediction_nonr.json',
 });
 
 /**
