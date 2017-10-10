758cdfa9eb3799a1c194acd9c200ffd74104e57b
Maciej Derulski <macder7@gmail.com>
2017-10-10 02:56:19 -0400

    Search WIP refactoring with recompose

--- src/modules/search/containers/SearchFormContainer2.js
+++ src/modules/search/containers/SearchFormContainer2.js
@@ -1,29 +1,57 @@
 import React from 'react';
+import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import { compose } from 'recompose';
+import { Dropdown } from 'semantic-ui-react'
+import { withDataOnInit, hideIfNoData, withSpinnerWhileLoading } from '../../core/enhancers';
 import { BaseComponent } from '../components';
+import * as selector from '../selectors';
+import * as action from '../actions';
 
-const TestComponent = props => (
-  <div className="c-test">
-    <p>Test</p>
-  </div>
-);
+const countryOptions = [
+  {key: 'ca', value: 'ca', text: 'Canada'},
+  {key: 'pl', value: 'pl', text: 'Poland'},
+];
 
-const enhance = compose(
-  TestComponent
-  /*connect(
+const mapStateToProps = state => ({
+  data: selector.getRouteList(state),
+  fetching: selector.isRouteFieldFetching(state),
+});
+
+const mapDispatchToProps = dispatch => ({
+  requestFetch: () => dispatch(action.loadRoutesRequest()),
+});
+
+const shouldFetchData = props =>
+  (!props.data && !props.fetching)
+
+const mergeProps = (stateProps, dispatchProps) => ({
+  requestData: (shouldFetchData(stateProps)) && (
+    () => dispatchProps.requestFetch()
+  ),
+  fetching: stateProps.fetching,
+  data: stateProps.data,
+  placeholder: 'Route number or name',
+
+});
+
+const RouteField = compose(
+  connect(
     mapStateToProps,
     mapDispatchToProps,
     mergeProps,
   ),
-  withDataOnUpdate,
+  withDataOnInit,
   withSpinnerWhileLoading,
   hideIfNoData,
-  withEmptyPredictions,
-  withSinglePrediction,
-  withSingeRouteMultiPredictions,
-  withMultiRouteMultiPredictions,
-  withMultiRouteMixedPredictions,*/
-);
+)(({ placeholder }) => (
+  <Dropdown placeholder={placeholder} fluid search selection options={countryOptions} />
+));
 
-export default enhance(BaseComponent);
+const SearchForm = () => (
+  <div>
+    <RouteField />
+  </div>
+)
+
+export default (SearchForm);
