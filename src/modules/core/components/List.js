2ae895df1979048529c1528d34a7750954638c1d
Maciej Derulski <macder7@gmail.com>
2017-10-11 02:41:20 -0400

    core - list no longer using material ui

--- src/modules/core/components/List.js
+++ src/modules/core/components/List.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { List as MaterialList, ListItem } from 'material-ui/List';
 
 const List = props => {
   const items = props.items.map(value =>
-    <ListItem primaryText={value.text} key={value.id} />
+    <li key={value.id}>{value.text} </li>
   );
   return (
-    <MaterialList>
+    <ul>
       {items}
-    </MaterialList>
+    </ul>
   )
 }
 
