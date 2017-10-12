f791b3f88715e60fb732d1be191a0620a52d0fa3
Maciej Derulski <macder7@gmail.com>
2017-10-12 02:01:38 -0400

    temp styles for search fields - proper styling will be seperate project

--- src/modules/search/components/DropdownField.js
+++ src/modules/search/components/DropdownField.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Dropdown } from 'semantic-ui-react'
+import './DropdownField.scss';
 
 const DropdownField = ({ data, placeholder, onChange }) => (
   <div className='c-input__dropdown'>
