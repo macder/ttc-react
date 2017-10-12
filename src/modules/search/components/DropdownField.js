0a5c61d56bb8bede5c5810699c1a00fbd1ae9d46
Maciej Derulski <macder7@gmail.com>
2017-10-12 00:29:44 -0400

    fixes #54 - change handler only fires when value changes

--- src/modules/search/components/DropdownField.js
+++ src/modules/search/components/DropdownField.js
@@ -2,15 +2,18 @@ import React from 'react';
 import { Dropdown } from 'semantic-ui-react'
 
 const DropdownField = ({ data, placeholder, onChange }) => (
-  <Dropdown
-    placeholder={placeholder}
-    options={data}
-    onChange={onChange}
-    fluid
-    search
-    scrolling
-    selection
-  />
+  <div className='c-input__dropdown'>
+    <Dropdown
+      placeholder={placeholder}
+      options={data}
+      onChange={onChange}
+      selectOnBlur={false}
+      fluid
+      search
+      scrolling
+      selection
+    />
+  </div>
 );
 
 export default (DropdownField);
