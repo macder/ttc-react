175a270180a6e78252f67033512d0d0c112dc34d
Maciej Derulski <macder7@gmail.com>
2017-10-15 03:12:23 -0400

    RouteFieldContainer - use withPropsOnChange and minor cleanup

--- src/modules/search/containers/RouteFieldContainer.js
+++ src/modules/search/containers/RouteFieldContainer.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { compose, mapProps, withHandlers, withStateHandlers } from 'recompose';
+import { compose, withHandlers, withPropsOnChange, withStateHandlers } from 'recompose';
 import { DropdownField } from '../components';
 import { withDataOnInit, hideIfNoData, withSpinnerWhileLoading } from '../../core/enhancers';
 import { getRouteList, isRouteListFetching } from '../selectors';
@@ -12,20 +12,18 @@ const mapStateToProps = state => ({
 });
 
 const mapDispatchToProps = dispatch => ({
-  requestFetch: () => dispatch(loadRoutesRequest()),
+  requestData: () => dispatch(loadRoutesRequest()),
   requestRouteConfig: (route) => dispatch(loadRouteConfigRequest(route)),
   routeSelected: (route) => dispatch(selectedRoute(route)),
 });
 
 const mergeProps = (stateProps, dispatchProps, ownProps) => ({
+  ...stateProps,
+  ...dispatchProps,
+  ...ownProps,
   requestData: (!stateProps.data && !stateProps.fetching) && (
-    () => dispatchProps.requestFetch()
+    () => dispatchProps.requestData()
   ),
-  requestRouteConfig: dispatchProps.requestRouteConfig,
-  routeSelected: dispatchProps.routeSelected,
-  fetching: stateProps.fetching,
-  data: stateProps.data,
-  ...ownProps
 });
 
 const RouteFieldContainer = compose(
@@ -37,12 +35,12 @@ const RouteFieldContainer = compose(
   withDataOnInit,
   withSpinnerWhileLoading,
   hideIfNoData,
-  mapProps(({ data, placeholder, requestRouteConfig, routeSelected }) => ({
-    data: data.toArray().map(item => item.toObject()),
-    placeholder,
-    routeSelected,
-    requestRouteConfig,
-  })),
+  withPropsOnChange(
+    ['data'],
+    ({ data }) => ({
+      data: data.toArray().map(item => item.toObject()),
+    })
+  ),
   withStateHandlers({ searchQuery: '' },{
     onSearchChange: ({ searchQuery }) => (event, data) => ({
       searchQuery: data.searchQuery
