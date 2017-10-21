c45c4b3a52e5052b10ad43bcbe271a7a7c13d69c
Maciej Derulski <macder7@gmail.com>
2017-10-21 05:19:10 -0400

    Search - new model file that defines data structures with methods

---
+++ src/modules/search/model.js
@@ -0,0 +1,44 @@
+import { fromJS, Map, OrderedSet, Record } from 'immutable';
+
+const routeRecord = Record({
+  id: '',
+  title: '',
+});
+
+const stopRecord = Record({
+  id: '',
+  title: '',
+  lat: '',
+  lon: '',
+});
+
+const directionRecord = Record({
+  id: '',
+  routeId: '',
+  title: '',
+  name: '',
+  stops: new Map(),
+});
+
+/**
+ * Creates a route record from map
+ * @param {Immutable.Map}
+ * @return {Immutable.Record}
+ */
+const createRouteRecord = item => new routeRecord({
+  id: item.get('tag'),
+  title: item.get('title'),
+});
+
+/**
+ * Callback for successfull data fetch for route list
+ * Normalizes and converts response data to immutable map
+ * @param {array} data Array of route objects
+ * @return {Immutable.Map}
+ */
+export const createRouteMap = data => (
+  new Map(fromJS(data).map(item => [
+    item.get('tag'),
+    createRouteRecord(item),
+  ])).set('results', new OrderedSet(data.map(item => item.tag)))
+);
