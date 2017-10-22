585a1d98481a3e509ab2c39021b1ac85ba14ad9b
Maciej Derulski <macder7@gmail.com>
2017-10-22 03:25:08 -0400

    WIP - routeField populates with new routeList data

--- src/modules/search/containers/RouteFieldContainer.js
+++ src/modules/search/containers/RouteFieldContainer.js
@@ -1,32 +1,39 @@
 import { connect } from 'react-redux';
-import { compose, withHandlers, withPropsOnChange } from 'recompose';
+import { compose, lifecycle, withHandlers, withPropsOnChange } from 'recompose';
 import { DropdownField } from '../components';
 import { withDataOnInit, hideIfNoData, withSpinnerWhileLoading } from '../../core/enhancers';
-import { getRouteList, getSelectedRoute, isRouteListFetching } from '../selectors';
+import { getRouteListForDropdown/*getRouteList, getSelectedRoute, isRouteListFetching*/ } from '../selectors';
 import { loadRoutesRequest, loadRouteConfigRequest, selectedRoute } from '../actions';
+import { requestRouteList } from '../../../data/entities/actions';
 
-const mapStateToProps = (state, ownProps) => ({
-  data: getRouteList(state),
-  fetching: isRouteListFetching(state),
-  selected: getSelectedRoute(state),
-});
+// console.dir(fetchRouteListIfNeeded);
+const mapStateToProps = (state, ownProps) => {
+// console.dir(state);
+// console.dir(getRouteList(state));
+// getRouteListForDropdown(state)
+return {
+  data: getRouteListForDropdown(state),
+  /*fetching: isRouteListFetching(state),
+  selected: getSelectedRoute(state),*/
+};
+}
 
 const mapDispatchToProps = dispatch => ({
-  requestData: () => dispatch(loadRoutesRequest()),
+  /*requestData: () => dispatch(loadRoutesRequest()),
   requestRouteConfig: route => dispatch(loadRouteConfigRequest(route)),
-  routeSelected: route => dispatch(selectedRoute(route)),
+  routeSelected: route => dispatch(selectedRoute(route)),*/
 });
 
-const valueFromURL = (isInitLoad, value, dispatchProps) => {
+/*const valueFromURL = (isInitLoad, value, dispatchProps) => {
   if (isInitLoad && value) {
     dispatchProps.routeSelected(value);
     dispatchProps.requestRouteConfig(value);
   }
   return value;
-};
+};*/
 
 const mergeProps = (stateProps, dispatchProps, ownProps) => ({
-  ...dispatchProps,
+  /*...dispatchProps,
   requestData: (!stateProps.data && !stateProps.fetching) && (
     () => dispatchProps.requestData()
   ),
@@ -38,17 +45,24 @@ const mergeProps = (stateProps, dispatchProps, ownProps) => ({
     dispatchProps,
   ),
   placeholder: ownProps.placeholder,
-  fetching: stateProps.fetching,
+  fetching: stateProps.fetching,*/
 });
 
 const RouteFieldContainer = compose(
   connect(
     mapStateToProps,
-    mapDispatchToProps,
-    mergeProps,
+    // mapDispatchToProps,
+    // mergeProps,
   ),
-  withDataOnInit,
-  withSpinnerWhileLoading,
+  lifecycle({
+    componentDidMount() {
+      const { dispatch } = this.props
+      dispatch(requestRouteList())
+    },
+  }),
+  //withDataOnInit,
+  //withSpinnerWhileLoading,
+
   hideIfNoData,
   withPropsOnChange(
     ['data'],
@@ -56,13 +70,14 @@ const RouteFieldContainer = compose(
       data: data.toArray().map(item => item.toObject()),
     }),
   ),
-  withHandlers({
+
+  /*withHandlers({
     onChange: props => (e, data) => {
       props.routeSelected(data.value);
       props.requestRouteConfig(data.value);
       props.historyReplace(`/${data.value}`);
     },
-  }),
+  }),*/
 )(DropdownField);
 
 export default (RouteFieldContainer);
