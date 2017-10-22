026180424109687a911dc8199bbb3fb49c80924e
Maciej Derulski <macder7@gmail.com>
2017-10-22 05:16:19 -0400

    Search - delete legacy reducers

--- src/modules/search/reducers/index.js
+++
@@ -1,5 +0,0 @@
-import routeFieldReducer from './routeFieldReducer';
-import directionFieldReducer from './directionFieldReducer';
-import stopFieldReducer from './stopFieldReducer';
-
-export default { routeFieldReducer, directionFieldReducer, stopFieldReducer };
