4d9c6a1b395d43092e220fa0f14dd43564090e60
Maciej Derulski <macder7@gmail.com>
2017-10-24 13:01:33 -0400

    delete predictions module - creating new full re-write

--- src/modules/predictions/components/index.js
+++
@@ -1,4 +0,0 @@
-export { default as BaseComponent } from './BaseComponent';
-export { default as hasEmptyPredictions } from './hasEmptyPredictions';
-export { default as hasSingeRoutePredictions } from './hasSingeRoutePredictions';
-export { default as hasMultiRoutePredictions } from './hasMultiRoutePredictions';
--- src/modules/predictions/index.js
+++
@@ -1,7 +0,0 @@
-import * as actions from './actions';
-// import * as constants from './constants';
-import * as reducer from './reducer';
-
-export { PredictionsContainer } from './containers';
-export { default as PredictionsSagas } from './sagas';
-export default { actions, reducer };
