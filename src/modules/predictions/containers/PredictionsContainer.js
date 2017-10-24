a7f9ac54bcbc9584cc85eed42f8185d87608977d
Maciej Derulski <macder7@gmail.com>
2017-10-24 19:11:54 -0400

    Predictions - container now loads data

--- src/modules/predictions/containers/PredictionsContainer.js
+++ src/modules/predictions/containers/PredictionsContainer.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { compose, lifecycle } from 'recompose';
+import { compose, lifecycle, withPropsOnChange } from 'recompose';
 import { Predictions } from '../components';
+import { getPredictionForList, getSelectedRoute, getSelectedStop, isPredictionFetching } from '../selectors';
 import { requestPrediction } from '../../../data/entities/actions';
+import { withSpinnerWhileLoading, hideIfNoData } from '../../core/enhancers';
+
+const shouldFetchData = props =>
+  (props.route && props.stop && !props.data && !props.fetching && !props.error);
 
 const mapStateToProps = (state, ownProps) => ({
+  data: getPredictionForList(state),
+  fetching: isPredictionFetching(state),
+  route: getSelectedRoute(state),
+  stop: getSelectedStop(state),
+})
 
-});
 
 const mapDispatchToProps = dispatch => ({
   action: {
-    requestPrediction: () => dispatch(requestPrediction()),
+    requestPrediction: (route, stop) => dispatch(requestPrediction(route, stop)),
   },
 });
 
@@ -18,14 +27,22 @@ const PredictionsContainer = compose(
   connect(
     mapStateToProps,
     mapDispatchToProps,
-    // mergeProps,
   ),
   lifecycle({
-    componentDidMount() {
-      const { action } = this.props;
-      action.requestPrediction();
+    componentWillReceiveProps(nextProps) {
+      const { action, route, stop, data, fetching } = nextProps;
+      (route && stop && !data && !fetching) &&
+        action.requestPrediction(route, stop);
     },
   }),
+  withSpinnerWhileLoading,
+  hideIfNoData,
+  withPropsOnChange(
+    ['data'],
+    ({ data }) => (data) && ({
+      data: data.toArray().map(item => item.toObject())
+    })
+  ),
 )(Predictions);
 
 export default (PredictionsContainer);
