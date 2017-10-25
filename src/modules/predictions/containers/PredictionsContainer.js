58cd5aeadfe1977035219171374ef6f7767a8a84
Maciej Derulski <macder7@gmail.com>
2017-10-24 20:20:28 -0400

    Predictions - optimize render by only passing used props

--- src/modules/predictions/containers/PredictionsContainer.js
+++ src/modules/predictions/containers/PredictionsContainer.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { compose, lifecycle, withPropsOnChange } from 'recompose';
+import { compose, lifecycle, mapProps, withPropsOnChange } from 'recompose';
 import { Predictions } from '../components';
 import {
   getPredictionForList, getSelectedRoute,
@@ -45,6 +45,9 @@ const PredictionsContainer = compose(
     ({ data }) => (data) && ({
       data: data.toArray().map(item => item.toObject())
     })
+  ),
+  mapProps(
+    ({ data }) => ({ data })
   ),
 )(Predictions);
 
