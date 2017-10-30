a85119c77c9bbe71c7353b2c753457382b64b859
Maciej Derulski <macder7@gmail.com>
2017-10-30 04:25:15 -0400

    core - ErrorMessage - use semantic-ui message box with icon

--- src/modules/core/components/ErrorMessage.jsx
+++ src/modules/core/components/ErrorMessage.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Message, Icon } from 'semantic-ui-react'
+
+import 'semantic-ui-css/components/message.min.css';
+import 'semantic-ui-css/components/icon.min.css';
 
 const ErrorMessage = ({ error }) => (
   <div className="c-error-message">
-    <p>{error.message}</p>
+    <Message error icon>
+      <Icon name='warning circle' />
+      <Message.Content>
+        <Message.Header>Error, please try again</Message.Header>
+        <p>{error.message}</p>
+      </Message.Content>
+    </Message>
   </div>
 );
 
